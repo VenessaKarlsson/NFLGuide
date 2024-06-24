e42942791d118005c21be5483ee2d33de270a144
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2024-06-24 20:55:36 +0200

    navbar bootstrap

--- nfl/app/page.tsx
+++ nfl/app/page.tsx
@@ -1,9 +1,48 @@
-import Image from 'next/image'
+'use client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Link from 'next/link'
+
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Offcanvas from 'react-bootstrap/Offcanvas'
 
 export default function Home() {
   return (
     <main>
-      <h1>hej</h1>
+      <Navbar expand="lg" className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-lg"
+            aria-labelledby="offcanvasNavbarLabel-expand-lg"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link>
+                  <Link href={'#'}>Home</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link href={'/about'}>About</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link href={'/teams'}>Teams</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link href={'/quiz'}>Quiz</Link>
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </main>
   )
 }
