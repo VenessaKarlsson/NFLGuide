e5034241777f00ba92f12be527b1eafee4e27af8
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2024-06-27 09:26:46 +0200

    navbar fixed

--- nfl/app/page.tsx
+++ nfl/app/page.tsx
@@ -1,40 +1,5 @@
 'use client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Link from 'next/link'
-
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Offcanvas from 'react-bootstrap/Offcanvas'
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar expand="lg" className="bg-body-tertiary mb-3">
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar-expand-lg"
-            aria-labelledby="offcanvasNavbarLabel-expand-lg"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
-                Offcanvas
-              </Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/teams">Teams</Nav.Link>
-                <Nav.Link href="/quiz">Quiz</Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </main>
-  )
+  return <main>hej</main>
 }
