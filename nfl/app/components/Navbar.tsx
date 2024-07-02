75c24ed4c2898ebe434f5c2bd3b5faa3e63b6b33
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2024-07-02 18:37:53 +0200

    navbar done

--- nfl/app/components/Navbar.tsx
+++ nfl/app/components/Navbar.tsx
@@ -1,7 +1,8 @@
 'use client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Link from 'next/link'
-
+import './Navbar.css'
+import Image from 'next/image'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
@@ -11,28 +12,47 @@ import React from 'react'
 function Mynavbar() {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary mb-3">
+      <Navbar expand="lg" className="bg-body-tertiary mb-3 navbar-color">
         <Container fluid>
+          <Image
+            src="/images/NFL_logo.png"
+            alt="nfl logo"
+            width={35}
+            height={48}
+            className="navbar-logo"
+          />
           <Link href="/" passHref legacyBehavior>
-            <Navbar.Brand>Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Brand className="brand-links">NFL Guide</Navbar.Brand>
           </Link>
-          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
+          <Navbar.Toggle
+            aria-controls="offcanvasNavbar-expand-lg"
+            className="navbar-toggle"
+          />
           <Navbar.Offcanvas
             id="offcanvasNavbar-expand-lg"
             aria-labelledby="offcanvasNavbarLabel-expand-lg"
             placement="end"
+            className="offcanvas-style"
           >
-            <Offcanvas.Header closeButton>
+            <Offcanvas.Header closeButton className="offcanvas-closebutton">
               <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
-                Offcanvas
+                NFL Guide
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/teams">Teams</Nav.Link>
-                <Nav.Link href="/quiz">Quiz</Nav.Link>
+                <Nav.Link href="/" className="navbar-links">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="/about" className="navbar-links">
+                  About
+                </Nav.Link>
+                <Nav.Link href="/teams" className="navbar-links">
+                  Teams
+                </Nav.Link>
+                <Nav.Link href="/quiz" className="navbar-links">
+                  Quiz
+                </Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
