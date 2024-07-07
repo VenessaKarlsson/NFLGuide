4816efa1c88b24d7f9f55e2c2dbb1a636209dc52
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2024-07-07 21:37:00 +0200

    background

--- nfl/app/page.tsx
+++ nfl/app/page.tsx
@@ -3,5 +3,12 @@ import './globals.css'
 import Image from 'next/image'
 
 export default function Home() {
-  return <main className="main">hej</main>
+  return (
+    <main className="main">
+      <section className="homepage-overview">
+        {/* <h1>NFL Guide</h1>
+        <p>A place for you to learn & explore more about American Football</p> */}
+      </section>
+    </main>
+  )
 }
