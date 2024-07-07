bbc3dfa61e0a370540d4413430641279ff7db3d8
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2024-07-07 22:44:39 +0200

    homepage

--- nfl/app/page.tsx
+++ nfl/app/page.tsx
@@ -6,8 +6,18 @@ export default function Home() {
   return (
     <main className="main">
       <section className="homepage-overview">
-        {/* <h1>NFL Guide</h1>
-        <p>A place for you to learn & explore more about American Football</p> */}
+        <div className="homepage-main-container">
+          <h1 className="homepage-title">
+            NFL Guide <span>&#127944;</span>
+          </h1>
+          <p className="homepage-subtitle">
+            A place for you to learn & explore more about American Football
+          </p>
+        </div>
+        <p className="homepage-detail__text">
+          NFL Guide is your go-to website for eager new fans to learn more about
+          the game, players and the teams
+        </p>
       </section>
     </main>
   )
