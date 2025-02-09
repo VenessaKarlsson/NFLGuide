4e9b5b6ac53b2a2046e3ac2bb60bbeef9a3ae5b9
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2025-02-09 11:38:44 +0100

    homepage change

--- nfl/app/page.tsx
+++ nfl/app/page.tsx
@@ -44,6 +44,63 @@ export default function Home() {
       </section>
 
       {/* Exploring */}
+      <div className="exploring-outer-container">
+        <h2 className="exploring-title">
+          Huddle Up and See What We Got To Offer!
+        </h2>
+        <section className="exploring-main-conatiner">
+          <div className="exploring-container">
+            <div className="exploring-wrapper">
+              <div className="exploring-banner-image"></div>
+              <h1>Rules</h1>
+              <p>Know more about the rules</p>
+            </div>
+
+            <div className="button-wrapper">
+              <button className="exploring-button-outline exploring-button">
+                Go to Quiz
+              </button>
+              <button className="exploring-button-fill exploring-button">
+                Learn more
+              </button>
+            </div>
+          </div>
+
+          <div className="exploring-container">
+            <div className="exploring-wrapper">
+              <div className="exploring-banner-image2"></div>
+              <h1>Fantasy Team</h1>
+              <p>Put together your fantasy nfl team</p>
+            </div>
+
+            <div className="button-wrapper">
+              <button className="exploring-button-outline exploring-button">
+                Go to Quiz
+              </button>
+              <button className="exploring-button-fill exploring-button">
+                Learn more
+              </button>
+            </div>
+          </div>
+
+          <div className="exploring-container">
+            <div className="exploring-wrapper">
+              <div className="exploring-banner-image3"></div>
+              <h1>Quiz</h1>
+              <p>Put your NFL knowlage to the test</p>
+            </div>
+
+            <div className="button-wrapper">
+              <button className="exploring-button-outline exploring-button">
+                Go to Quiz
+              </button>
+              <button className="exploring-button-fill exploring-button">
+                Learn more
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
     </main>
   )
 }
